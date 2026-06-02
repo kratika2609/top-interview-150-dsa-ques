8ff313b0e668a80cc83f80c739e77590f29d10db
Kratika Singh <2k23.csds2311468@gmail.com>
2026-06-02 23:56:09 +0530

    Add files via upload

---
+++ removeElement.java
@@ -0,0 +1,19 @@
+//Time complexity O(n)
+//Space complexity O(1)
+//Pattern : Two Pointers / In-Place Array Modification
+
+import java.util.*;
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        return index;
+    }
+}
